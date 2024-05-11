ba92fa29efe9a6bb7a6dda1b8e19e47363b61a2c
hamza <hmzaerr@gmail.com>
2024-05-11 20:28:54 +0100

    edit index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,73 +21,96 @@ import OurStore from "./pages/user/OurStore";
 import ProductDetaile from "./pages/user/ProductDetaile";
 import BookCoaching from "./pages/user/BookCoaching";
 import Booking from "./pages/user/Booking";
+import { useNavigate } from "react-router-dom";
 
 import Chekout from "./pages/user/Chekout";
 import Test from "./pages/user/Test";
 import BookingPage from "./pages/user/BookingPage";
 import ChekoutFood from "./pages/user/ChekoutFood";
 import ErrorPage from "./components/ErrorPage";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+  if (user) {
+    console.log("he is a ", user.role);
+  } else {
+    console.log("no user found");
+    navigate("/login");
+  }
+
   return (
     <Routes>
-      <Route path="/test" element={<Test />} />
-      <Route path="/" element={<Layout />}>
-        <Route path="/chekout/product" element={<Chekout />} />
-        <Route path="/chekout/food" element={<ChekoutFood />} />
-        <Route path="/Clubs" element={<Clubs />} />
-        <Route path="/forget" element={<ForgetPass />} />
-        <Route path="/forget/:forgetToken" element={<EditPass />} />
-        <Route path="/confirm/:activation" element={<ConfirmPage />} />
-        <Route path="/personal_training" element={<PersonaleTraining />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/spa" element={<Spa />} />
-        <Route path="/nurition" element={<Nurition />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/espace" element={<Espace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/store" element={<OurStore />} />
-        <Route path="/store/:id" element={<ProductDetaile />} />
-        <Route path="/findCoach" element={<BookCoaching />} />
-        <Route path="/findCoach/:id" element={<Booking />} />
-        <Route
-          path="/spa/massage"
-          element={
-            <BookingPage
-              image={massage}
-              title1={"massage"}
-              title2={"hhhh"}
-              para={"hello baby"}
-              type={"massage"}
+      {!user && (
+        <>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </>
+      )}
+      {/*i will add route of coach 
+      /add profile view  */}
+
+      {user && (
+        <>
+          <Route path="/" element={<Layout />}>
+            <Route path="/chekout/product" element={<Chekout />} />
+            <Route path="/chekout/food" element={<ChekoutFood />} />
+            <Route path="/Clubs" element={<Clubs />} />
+            <Route path="/forget" element={<ForgetPass />} />
+            <Route path="/forget/:forgetToken" element={<EditPass />} />
+            <Route path="/confirm/:activation" element={<ConfirmPage />} />
+            <Route path="/personal_training" element={<PersonaleTraining />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/spa" element={<Spa />} />
+            <Route path="/nurition" element={<Nurition />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/espace" element={<Espace />} />
+
+            <Route path="/store" element={<OurStore />} />
+            <Route path="/store/:id" element={<ProductDetaile />} />
+            <Route path="/findCoach" element={<BookCoaching />} />
+            <Route path="/findCoach/:id" element={<Booking />} />
+            <Route
+              path="/spa/massage"
+              element={
+                <BookingPage
+                  image={massage}
+                  title1={"massage"}
+                  title2={"hhhh"}
+                  para={"hello baby"}
+                  type={"massage"}
+                />
+              }
             />
-          }
-        />
-        <Route
-          path="/spa/keni"
-          element={
-            <BookingPage
-              image={keni}
-              title1={"keni"}
-              title2={"hhhh"}
-              para={"hello baby"}
-              type={"kenie"}
+            <Route
+              path="/spa/keni"
+              element={
+                <BookingPage
+                  image={keni}
+                  title1={"keni"}
+                  title2={"hhhh"}
+                  para={"hello baby"}
+                  type={"kenie"}
+                />
+              }
             />
-          }
-        />
-        <Route
-          path="/spa/spa"
-          element={
-            <BookingPage
-              image={spa}
-              title1={"test"}
-              title2={"hhhh"}
-              para={"hello baby"}
-              type={"spa"}
+            <Route
+              path="/spa/spa"
+              element={
+                <BookingPage
+                  image={spa}
+                  title1={"test"}
+                  title2={"hhhh"}
+                  para={"hello baby"}
+                  type={"spa"}
+                />
+              }
             />
-          }
-        />
-      </Route>
+          </Route>
+        </>
+      )}
+
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
